7aab7d019f9ef5da60200db71da0cd8eac2bd08d
Luis angel ponce <poncealvarezluisangel4@gmail.com>
2023-05-20 19:14:13 -0600

    v1

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from distutils.core import setup
 import py2exe
 
-setup(
-    console=['app.py'],
-    options={
-        "py2exe": {
-            "bundle_files": 1,
-            "compressed": True
-        }
-    },
-    zipfile=None
-)
+setup(console=['app.py'])
+
